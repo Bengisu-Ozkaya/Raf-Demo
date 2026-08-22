78a760df9c62304c5218b68d03b0f6e3d553685e
Bengisu-Ozkaya <2023141044@cumhuriyet.edu.tr>
2026-08-22 14:28:08 +0300

    Update constants.dart

--- Raf_Demo/raf_demo/lib/utils/constants.dart
+++ Raf_Demo/raf_demo/lib/utils/constants.dart
@@ -1,5 +1,4 @@
 // Backend sunucusunun temel URL'si.
 // LocalTunnel, ngrok gibi servisler her yeniden başlatıldığında URL değişebilir.
 // Bu yüzden burayı merkezi bir yerden yönetmek önemlidir.
-const String BASE_URL =
-    'http://192.168.1.107:3001'; // Port 3001 olarak düzeltildi
+const String BASE_URL = 'https://raf-demo-backend.onrender.com'; // Port 3001 olarak düzeltildi
